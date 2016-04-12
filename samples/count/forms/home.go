df525d60824b7d5c974990481278816859da7ec9
Trumae <trumae@gmail.com>
2016-04-12 14:07:10 -0300

    sample count

---
+++ samples/count/forms/home.go
@@ -0,0 +1,43 @@
+package forms
+
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/trumae/valente"
+	"github.com/trumae/valente/action"
+	"golang.org/x/net/websocket"
+)
+
+const htmlFormHome = `
+<h3>Home</h3>
+  <span id="count"></span>
+`
+
+//FormHome example
+type FormHome struct {
+	valente.FormImpl
+}
+
+//Initialize inits the Home Form
+func (form FormHome) Initialize(ws *websocket.Conn) valente.Form {
+	log.Println("FormHome Initialize")
+
+	action.HTML(ws, "content", htmlFormHome)
+
+	go func() {
+		i := 0
+		c := time.Tick(1 * time.Second)
+		for _ = range c {
+			i = i + 1
+			err := action.HTML(ws, "count", strconv.Itoa(i))
+			if err != nil {
+				log.Println("Error sending count ", err)
+				return
+			}
+		}
+	}()
+
+	return form
+}
---
+++ samples/count/forms/login.go
@@ -0,0 +1,62 @@
+package forms
+
+import (
+	"log"
+
+	"github.com/trumae/valente"
+	"github.com/trumae/valente/action"
+	"golang.org/x/net/websocket"
+)
+
+const htmlForm = `
+<h3>Login</h3>
+<p>
+User: admin<br/>
+Password: admin
+</p>
+<form>	
+
+   <label for="text-basic">User:</label>
+   <input name="text-basic" id="user" value="" type="text">
+
+   <label for="password">Password:</label>
+   <input name="password" id="pass" value="" autocomplete="off" type="password"/>
+
+	 <a href="javascript:sendEvent('loginOk')">Ok</a>
+</form>
+`
+
+//FormLogin is a form for auth
+type FormLogin struct {
+	valente.FormImpl
+}
+
+func loginOk(ws *websocket.Conn, app *valente.App, params []string) {
+	log.Println("Handling loginOk")
+
+	user, err := action.Get(ws, "user")
+	if err != nil {
+		log.Println(err)
+	}
+
+	pass, err := action.Get(ws, "pass")
+	if err != nil {
+		log.Println(err)
+	}
+
+	if user == "admin" && pass == "admin" {
+		action.HTML(ws, "loginName", "admin")
+		app.GoTo("home", nil)
+	}
+}
+
+//Initialize inits the login Form
+func (form FormLogin) Initialize(ws *websocket.Conn) valente.Form {
+	log.Println("FormLogin Initialize")
+
+	f := form.AddEventHandler("loginOk", loginOk)
+
+	action.HTML(ws, "content", htmlForm)
+
+	return f
+}
