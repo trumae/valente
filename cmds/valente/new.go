94c5d409d1b990e3260b40adcc2026445755b460
Trumae <trumae@gmail.com>
2016-12-24 19:13:00 -0200

    fix valente command

--- cmds/valente/new.go
+++ cmds/valente/new.go
@@ -19,14 +19,13 @@ import (
 	 "time"
 
    "github.com/labstack/echo"
-   "github.com/labstack/echo/engine/standard"
    "github.com/labstack/echo/middleware"
 	 "github.com/satori/go.uuid"
    "github.com/trumae/valente"
    "github.com/trumae/valente/status"
 	 "{{ . }}"
 
-   "golang.org/x/net/websocket"
+	 "github.com/gorilla/websocket"
 )
 
 //App is a Web Application representation
@@ -40,9 +39,11 @@ const gctime = 1
 var (
 	sessions map[string]*App
 	mutex    sync.Mutex
+
+	upgrader = websocket.Upgrader{}
 )
 
-//addiSession include a new app on sessions
+//addSession include a new app on sessions
 func addSession(key string, app *App) {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -96,49 +97,56 @@ func main() {
 
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
-    e.Use(middleware.Static("public"))
+    e.Static("/", "public")
 
     e.GET("/status", status.ValenteStatusHandler)
-    e.GET("/ws", standard.WrapHandler(websocket.Handler(func(ws *websocket.Conn) {
-		   err := websocket.Message.Send(ws, "__GETSESSION__")
-		   if err != nil {
-			   return
-		   }
+    e.GET("/ws", func(c echo.Context) error {
+        ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	      if err != nil {
+	              return err
+	      }
+	      defer ws.Close()
 
-		   idSession := ""
-		   err = websocket.Message.Receive(ws, &idSession)
-		   if err != nil {
-			   return
-		   }
+	      err = ws.WriteMessage(websocket.TextMessage, []byte("__GETSESSION__"))
+        if err != nil {
+                return err
+        }
 
-		   var app *App
-		   app = getSession(idSession)
-		   if app == nil {
-			    u1 := uuid.NewV4().String()
-					log.Println("New session", u1)
-			    app = &App{}
-					app.LastAccess = time.Now()
-			    addSession(u1, app)
-			    err := websocket.Message.Send(ws, u1)
-			    if err != nil {
-				    return
-			    }
-		      app.WebSocket(ws)
-		      app.Initialize()
-		   } else {
-				  app.LastAccess = time.Now()
-				  log.Println("Reusing session", idSession)
-			    err := websocket.Message.Send(ws, idSession)
-			    if err != nil {
-				    return
-			    }
-		      app.WebSocket(ws)
-			 }
-		   app.Run()
-    })))
+        _, bid, err := ws.ReadMessage()
+       if err != nil {
+                return err
+       }
+       idSession := string(bid)
+
+       var app *App
+       app = getSession(idSession)
+       if app == nil {
+               u1 := uuid.NewV4().String()
+               log.Println("New session", u1)
+               app = &App{}
+               app.LastAccess = time.Now()
+               addSession(u1, app)
+               err := ws.WriteMessage(websocket.TextMessage, []byte(u1))
+               if err != nil {
+                      return err
+               }
+               app.WebSocket(ws)
+               app.Initialize()
+       } else {
+               app.LastAccess = time.Now()
+               log.Println("Reusing session", idSession)
+               err := ws.WriteMessage(websocket.TextMessage, []byte(idSession))
+               if err != nil {
+                       return err
+               }
+               app.WebSocket(ws)
+       }
+       app.Run()
+	     return nil
+    })
 
 		log.Println("Server running")
-    e.Run(standard.New(":8000"))
+    e.Start(":8000")
 }
 
 `
