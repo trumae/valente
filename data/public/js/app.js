51e66489b5e6a79e59ac9515a419692274639bb9
trumae <trumae@gmail.com>
2017-03-07 02:25:42 -0300

    fix load script sparkline bug

--- data/public/js/app.js
+++ data/public/js/app.js
@@ -106,26 +106,22 @@ function isScriptLoaded(url) {
 }
 
 function loadScript( url, callback ) {
-  if (isScriptLoaded(url)) {
-    callback();
-  } else {
-    var script = document.createElement( "script" )
-      script.type = "text/javascript";
-    if(script.readyState) {  //IE
-      script.onreadystatechange = function() {
-        if ( script.readyState === "loaded" || script.readyState === "complete" ) {
-          script.onreadystatechange = null;
-          callback();
-        }
-      };
-    } else {  //Others
-      script.onload = function() {
+  var script = document.createElement( "script" )
+    script.type = "text/javascript";
+  if(script.readyState) {  //IE
+    script.onreadystatechange = function() {
+      if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+        script.onreadystatechange = null;
         callback();
-      };
-    }
-
-    script.src = url;
-    document.getElementsByTagName( "head" )[0].appendChild( script );
+      }
+    };
+  } else {  //Others
+    script.onload = function() {
+      callback();
+    };
   }
+
+  script.src = url;
+  document.getElementsByTagName( "head" )[0].appendChild( script );
 }
 
